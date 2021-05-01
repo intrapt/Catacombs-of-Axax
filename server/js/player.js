b77ad8b38f990de229fcf22cf189ed832b3b43e0
intrapt <alexjmoir@gmail.com>
2021-05-01 17:50:22 +0100

    Added dropItem

--- server/js/player.js
+++ server/js/player.js
@@ -16,7 +16,7 @@ function newPlayer(socket) {
         takeItem = (item) => {
             let inInv = false;
             for (i = 0; i < locationList[location].items.length; i++) {
-                if (item = locationList[location].items[i].id) {
+                if (item == locationList[location].items[i].id) {
                     locationList[location].items.splice(i, 1);
                     for (let j = 0; j < inventory.length; i++) {
                         if (item == inventory[j][0]) {
@@ -28,6 +28,32 @@ function newPlayer(socket) {
                     }
                 }
             }
+           /**
+            * Check if an item in the room has the same ID as the given item
+            * Then remove it from the room
+            * If same type of item is in inventory then increment counter for
+            * it by 1
+            * Else push item to inventory
+            */
+        },
+        dropItem = (item) => {
+            const itemObject = {
+                id: item.id,
+                loc: "on the floor"
+            };
+            for (i = 0; i < inventory.length; i++) {
+                if (item.id == inventory[i].id) {
+                    inventory.splice(i, 1);
+                    locationList(location).items.push(itemObject);
+                }
+            }
+            /**
+             * Create item object
+             * Check inventory for item
+             * If it exists then remove it from inventory then push item
+             * object to room
+             * TODO: Make dropItem() only drop one item if there are multiple in inventory
+             */
         },
     };
 
